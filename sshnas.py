2f681877284ead844d4bfec80cb164117a484cc3
nocow4bob <robert.ant.davis@gmail.com>
2013-11-03 12:35:04 -0500

    added options parsing

--- sshnas.py
+++ sshnas.py
@@ -3,6 +3,7 @@ import sys
 import Exscript
 import threading
 import Queue
+import argparse
 
 from Exscript.util.start import start
 from Exscript.util.interact import read_login
@@ -10,6 +11,8 @@ from Exscript.protocols import SSH2
 from Exscript import Host, Account
 from Exscript.util.file  import get_hosts_from_file
 from Exscript.util.start import quickstart
+from optparse import OptionParser
+
 
 def do_migration(host):
 	try:
@@ -80,83 +83,87 @@ def worker(queue, option):
 				outfile.write("[X] Invalid option for thread worker")
 		except Queue.Empty:
             		queue_full = False
-			
-
-print ""
-print "------- Welcome to RAD's Cisco migration script -------"
-print ""
-
-global outfile
-hosts = open(sys.argv[1]) 
-outfile = open('migration.log','w')
-#hosts = get_hosts_from_file(sys.argv[1])
-#accounts = [Account('migration', 'i1jdhci5hanbad75h')]
-account = Account('migration', 'i1jdhci5hanbad75h')
-option = raw_input("migrate (m) or cancel reloads (c): ")
+# Main function
+def main(threads,logfile,hosts_file,commands,username,password):
+	global outfile
+	hosts = open(hosts_file) 
+	outfile = open(logfile,'w')
+	account = Account(username, password)
+	option = raw_input("migrate (m) or cancel reloads (c): ")
  
-if option == 'm':
-	print "\nStarting migrations..."
-	outfile.write("\nStarting migrations...\n")
-	q = Queue.Queue()
-	for host in hosts:
-		q.put(host)
-	
-	threads = []
-	thread_count = 10
-	for i in range(thread_count):
-    		t = threading.Thread(target=worker, args = (q,option,))
-    		threads.append(t)
-	#	t.start()
-	# Start all threads
-	[x.start() for x in threads]
-	# Wait for all threads to finish before moving on
-	[x.join() for x in threads]
-	
-	
-	print ""
-	cancel = raw_input("Would you like to cancel the reloads? ")
-	if cancel == 'y':
-		hosts = open(sys.argv[1])
-		print "\nCancelling reloads..."
-		outfile.write("\nCancelling reloads...\n")
+	if option == 'm':
+		print "\nStarting migrations..."
+		outfile.write("\nStarting migrations...\n")
 		q = Queue.Queue()
 		for host in hosts:
 			q.put(host)
+	
 		threads = []
 		thread_count = 10
 		for i in range(thread_count):
-                	t = threading.Thread(target=worker, args = (q,'c',))
-        		threads.append(t)        	
+    			t = threading.Thread(target=worker, args = (q,option,))
+    			threads.append(t)
+		# Start all threads
+		[x.start() for x in threads]
+		# Wait for all threads to finish before moving on
+		[x.join() for x in threads]
+	
+	
+		print ""
+		cancel = raw_input("Would you like to cancel the reloads? ")
+		if cancel == 'y':
+			hosts = open(sys.argv[1])
+			print "\nCancelling reloads..."
+			outfile.write("\nCancelling reloads...\n")
+			q = Queue.Queue()
+			for host in hosts:
+				q.put(host)
+			threads = []
+			thread_count = 10
+			for i in range(thread_count):
+                		t = threading.Thread(target=worker, args = (q,'c',))
+        			threads.append(t)        	
+
+			# Start all threads
+        		[x.start() for x in threads]
+        		# Wait for all threads to finish before moving on
+        		[x.join() for x in threads]
+	
+		else:
+			print "Migration complete"
+
+	elif option == 'c':
+		hosts = open(sys.argv[1])
+        	print "\nCancelling reloads..."
+		outfile.write("\nCancelling reloads...\n")
+        	q = Queue.Queue()
+		for host in hosts:
+			q.put(host)
+        	
+		threads = []
+		thread_count = 10
+		for i in range(thread_count):
+        		t = threading.Thread(target=worker, args = (q,option,))
+                	threads.append(t)
 
 		# Start all threads
         	[x.start() for x in threads]
         	# Wait for all threads to finish before moving on
-        	[x.join() for x in threads]
-	
+        	[x.join() for x in threads]	
 	else:
-		print "Migration complete"
+		print "[!] Not a valid option"
 
-elif option == 'c':
-	hosts = open(sys.argv[1])
-        print "\nCancelling reloads..."
-	outfile.write("\nCancelling reloads...\n")
-        q = Queue.Queue()
-	for host in hosts:
-		q.put(host)
-        	
-	threads = []
-	thread_count = 10
-	for i in range(thread_count):
-        	t = threading.Thread(target=worker, args = (q,option,))
-                threads.append(t)
+	print ""
 
-	# Start all threads
-        [x.start() for x in threads]
-        # Wait for all threads to finish before moving on
-        [x.join() for x in threads]	
-else:
-	print "[!] Not a valid option"
 
-print ""
-print "------ RAD's Cisco migration script complete ------"
-
+# Start	here
+if __name__ == "__main__":		
+	parser = argparse.ArgumentParser(version="%prog 1.0",description="A multi-threaded SSH network automation script")
+        parser.add_argument('hosts', help='Hosts file', metavar='HOSTS_FILE')
+	parser.add_argument('commands', help='Commands file', metavar='COMMANDS_FILE')
+        parser.add_argument("-l", "--log-file", dest='logfile', help="logfile name", metavar="LOGFILE", default="sshnas.log")
+	parser.add_argument("-t", "--threads", dest='threads', help="number of threads (1-100)", type=int, metavar='THREADS', default=10)
+	parser.add_argument("-u", "--username", dest='username', help='Username for SSH login', metavar='USERNAME', default='admin')
+	parser.add_argument("-p", "--password", dest='password', help='Password for SSH login', metavar='PASSWORD', default='admin')
+	args = parser.parse_args()
+	main(args.threads,args.logfile,args.hosts,args.commands,args.username,args.password)
